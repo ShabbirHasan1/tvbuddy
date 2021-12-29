3455306258101b9d1d1013c2a2ae571a8109b948
Ossian Winter <ossian-winter@riseup.net>
2021-12-29 06:38:47 +0100

    Replace eval with numexpr.evaluate
    Fixes #1

--- internal/filtering.py
+++ internal/filtering.py
@@ -1,21 +1,27 @@
-def filter_custom(markets, expression):
+import numexpr
+
+
+def filter_markets(markets, predicate):
     filtered_markets = []
 
     for market in markets:
-        passes_filter = eval(expression, {"market": market})
-        if passes_filter:
+        if predicate(market):
             filtered_markets.append(market)
 
     return filtered_markets
 
 
+def filter_custom(markets, expression):
+    return filter_markets(markets, lambda market: numexpr.evaluate(expression, market))
+
+
 def filter_volume(markets):
-    return filter_custom(markets, "market['usd_volume']>0")
+    return filter_markets(markets, lambda market: market["usd_volume"] > 0)
 
 
 def filter_price(markets):
-    return filter_custom(markets, "market['price']>0")
+    return filter_markets(markets, lambda market: market["price"] > 0)
 
 
 def filter_perpetuals(markets):
-    return filter_custom(markets, "market['name'].endswith('PERP')")
+    return filter_markets(markets, lambda market: market["name"].endswith("PERP"))
